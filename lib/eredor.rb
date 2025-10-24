8cfa6e6e2f68099435e35eca36aa9c341a9c5f97
guilherme <devguilhermeribeiro000@gmail.com>
2025-10-23 22:35:01 -0300

    Refactoring code
    Refactoring rounter and controller to improve the code

--- lib/eredor.rb
+++ lib/eredor.rb
@@ -1,4 +1,4 @@
-# frozen_string_literal: true
+# frozen_string_literal: false
 
 require_relative "eredor/version"
 
@@ -26,18 +26,17 @@ module Eredor
     def handle
       method = @request.request_method.to_sym
       path   = @request.path
-      body   = %w[POST PUT PATCH].include?(method.to_s) ? @request.POST : nil
-      query  = @request.GET
+      params = @request.params
 
       @routes[method].each do |route|
         if route[:pattern]
           match = route[:pattern].match(path)
           next unless match
 
-          params = extract_params(match, route[:keys])
-          return call_handler(route, params, body, query)
+          params.merge!(extract_params(match))
+          return call_handler(route, params)
         elsif route[:path] == path
-          return call_handler(route, {}, body, query)
+          return call_handler(route, params)
         end
       end
 
@@ -49,34 +48,23 @@ module Eredor
     def register_route(method, path, &block)
       raise(StandardError, "A block must be given") unless block_given?
 
-      keys = []
-      regex_path = path.gsub(/:(\w+)/) do |match|
-        keys << match[1..-1].to_sym
-        "(?<#{keys.last}>[^/]+)"
-      end
+      regex_path = path.gsub(/:(\w+)/) { |m| "(?<#{m[1..-1]}>[^/]+)" }
+      pattern = /\A#{regex_path}\z/
 
-      route = { path: path, handler: block }
-      route[:pattern] = /\A#{regex_path}\z/ if keys.any?
-      route[:keys] = keys if keys.any?
-
-      @routes[method] << route
-    end
-
-    def extract_params(match, keys)
-      keys.each_with_object({}) do |key, h|
-        h[key] = match[key.to_s]
-      end
-    end
-
-    def call_handler(route, params, body, query)
-      env = {
-        params: params,
-        body: body,
-        query: query
+      @routes[method] << {
+        path: path,
+        pattern: path.include?(":") ? pattern : nil,
+        handler: block
       }
-      result = route[:handler].call(env)
+    end
 
-      # Permitir retorno flexível
+    def extract_params(match)
+      match.named_captures.transform_keys(&:to_sym)
+    end
+
+    def call_handler(route, params)
+      result = route[:handler].call(params)
+
       case result
       when Array || Rack::Response then result # já está no formato [status, headers, body]
       when String then [200, { "content-type" => "text/html" }, [result]]
@@ -91,11 +79,10 @@ module Eredor
   end
 
   class BaseController
-    attr_reader :params, :request
+    attr_reader :params
 
-    def initialize(request)
-      @params = request.params
-      @request = request
+    def initialize(params)
+      @params = params
     end
 
     def render(file)
@@ -107,13 +94,12 @@ module Eredor
     private
 
     def get_view(file)
-      File.read("../views/#{controller_name}/#{file}.html.erb")
+      File.read("./app/views/#{controller_name}/#{file}.html.erb")
     end
 
     def controller_name
-      controller = self.class.name
-      controller.gsub!(/Controller/, "")
-      controller.downcase!
+      controller = self.class.name.to_s
+      controller.gsub(/Controller/, "").downcase
     end
   end
 end
